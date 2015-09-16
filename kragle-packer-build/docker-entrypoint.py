86b9aba6cc515b2fb31df72911abf26542d70ee5
Joseph Callen <jcpowermac@gmail.com>
2015-09-15 23:09:32 -0400

    Add new python script to support adding submgr username and password

---
+++ kragle-packer-build/docker-entrypoint.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+
+from jinja2 import Environment, FileSystemLoader 
+from subprocess import Popen
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--username', dest='username', help='Username used for subscription-manage', required=True)
+parser.add_argument('--password', dest='password', help='Password used for subscription-maange', required=True)
+parser.add_argument('--packer', dest='packer', help='Options required for packer', required=True)
+args = parser.parse_args()
+
+
+#env = Environment(loader=FileSystemLoader('/opt/kragle/templates'))
+env = Environment(loader=FileSystemLoader('./templates'))
+template = env.get_template('ks-isolinux-packer.cfg.j2')
+
+output = template.render(submgr_username=args.username, submgr_password=args.password)
+
+#with open('/opt/kragle/kickstart-files/ks-isolinux-packer.cfg', 'w') as f:
+with open('./kickstart-files/ks-isolinux-packer.cfg', 'w') as f:
+	f.write(output)
+
+packer_args = args.packer.split()
+
+packer_args.insert(0, '/opt/packer/packer')
+Popen(packer_args)
+
